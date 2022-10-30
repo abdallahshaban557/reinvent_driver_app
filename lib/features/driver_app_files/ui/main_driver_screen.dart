4c5ff2f72a8b78bca81646416370ea85dd9b386c
Abdallah Shaban <abdallah.w.shaban@gmail.com>
2022-10-29 18:34:31 -0700

    removed unncecessary dependencies

--- lib/features/driver_app_files/ui/main_driver_screen.dart
+++ lib/features/driver_app_files/ui/main_driver_screen.dart
@@ -1,5 +1,4 @@
 import 'package:amplify_api/model_mutations.dart';
-import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
